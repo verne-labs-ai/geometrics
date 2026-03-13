b663c033ed87b134d5de009ee82795348da4af4d
Neil Nie <yongyang.nie@gmail.com>
2026-03-12 19:10:46 -0700

    Move SE(3) functions to separate module, remove duplicates from pose_interpolation

--- geometrics/interpolation/__init__.py
+++ geometrics/interpolation/__init__.py
@@ -1,5 +1,5 @@
 # Auto-generated __init__.py
-from geometrics.interpolation.pose_interpolation import (
+from geometrics.interpolation.se3_interpolation import (
     interpolate_se3,
     interpolate_se3_trajectory,
 )
